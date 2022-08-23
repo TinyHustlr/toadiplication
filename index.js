870f4b2113e695f0e5fad5e1dd63dfc636a4fd8e
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-08-23 22:43:05 +1000

    New format

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
+let answer = 0
 let count1 = 0
 let count2 = 0
-let answer = 0
+let count1El = document.querySelector("#count1-el")
+let count2El = document.querySelector("#count2-el")
+let answerEl = document.querySelector("#answer-el")
 
-let count1El = document.getElementById("count1-el")
-let count2El = document.getElementById("count2-el")
-let answerEl = document.getElementById("answer-el")
+function newNumbers() {
+let randomNumber = Math.floor(Math.random() * 16) + 1
+return randomNumber
+}
 
-function add1() {
-    count1 += + 1
+function newQuestion() {
+    count1 = newNumbers()
+    count2 = newNumbers()
     count1El.textContent = count1
+    count2El.textContent = " X " + count2
+    answerEl.textContent = "= " + 0
 }
 
-function subtract1() {
-    count1 += -1
-    count1El.textContent = count1
-}
-
-function add2() {
-    count2 += + 1
-    count2El.textContent = count2
-}
-
-function subtract2() {
-    count2 += -1
-    count2El.textContent = count2
-}
 
 function multiply() {
     let answer = count1 * count2
     console.log(answer)
-    answerEl.textContent = answer
+    answerEl.textContent = "= " + answer
 }
